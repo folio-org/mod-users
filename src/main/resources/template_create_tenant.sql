606d44675c373a6ac78c8f71205addb5ccd1f952
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2017-02-01 16:10:21 -0500

    Fixing errors in sql template

--- src/main/resources/template_create_tenant.sql
+++ src/main/resources/template_create_tenant.sql
@@ -1,9 +1,9 @@
-CREATE ROLE myuniversity PASSWORD 'myuniversity' NOSUPERUSER NOCREATEDB INHERIT LOGIN;
+CREATE ROLE myuniversity_mymodule PASSWORD 'myuniversity' NOSUPERUSER NOCREATEDB INHERIT LOGIN;
 
-CREATE SCHEMA myuniversity_mymodule AUTHORIZATION myuniversity;
+CREATE SCHEMA myuniversity_mymodule AUTHORIZATION myuniversity_mymodule;
 
-CREATE EXTENSION IF NOT EXISTS 'pgcrypto';
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE IF NOT EXISTS myuniversity_mymodule.users (_id UUID PRIMARY KEY DEFAULT gen_random_uuid(), jsonb JSONB NOT NULL);
 
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA myuniversity_mymodule TO myuniversity;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA myuniversity_mymodule TO myuniversity_mymodule;
