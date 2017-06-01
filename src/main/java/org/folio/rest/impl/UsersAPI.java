ede0744693557bf0c946f228292e5a575eb420bd
shale <shale99@users.noreply.github.com>
2017-06-01 09:10:48 +0300

    MODUSERS-17

--- src/main/java/org/folio/rest/impl/UsersAPI.java
+++ src/main/java/org/folio/rest/impl/UsersAPI.java
@@ -521,8 +521,13 @@ public class UsersAPI implements UsersResource {
            }
          }
          else{
-           logger.error(check.cause().getLocalizedMessage(), check.cause());
-           handler.handle(io.vertx.core.Future.succeededFuture(-1));
+           Throwable t = check.cause();
+           logger.error(t.getLocalizedMessage(), t);
+           int retCode = -1;
+           if(t.getLocalizedMessage().contains("invalid input syntax for uuid")){
+             retCode = 0;
+           }
+           handler.handle(io.vertx.core.Future.succeededFuture(retCode));
          }
      });
    }
