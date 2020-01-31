1d8e30b67379820eb9087354b4a45bc0bd66f0bc
Roman_Barannyk <roman_barannyk@epam.com>
2020-01-31 17:06:56 +0200

    MODUSERS-174 create patron block condition API, add tests

---
+++ src/main/java/org/folio/rest/impl/PatronBlockConditionsAPI.java
@@ -0,0 +1,68 @@
+package org.folio.rest.impl;
+
+import java.util.Map;
+
+import javax.ws.rs.core.Response;
+
+import org.folio.rest.jaxrs.model.PatronBlockCondition;
+import org.folio.rest.jaxrs.resource.PatronBlockConditions;
+import org.folio.rest.persist.PgUtil;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Handler;
+
+public class PatronBlockConditionsAPI implements PatronBlockConditions {
+
+  private static final String PATRON_BLOCKS_CONDITIONS = "patron_blocks_conditions";
+
+  @Override
+  public void getPatronBlockConditions(int offset, int limit, String query,
+    String lang, Map<String, String> okapiHeaders,
+    Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+
+    PgUtil.get(PATRON_BLOCKS_CONDITIONS, PatronBlockCondition.class,
+      org.folio.rest.jaxrs.model.PatronBlockConditions.class, query, offset, limit,
+      okapiHeaders, vertxContext, GetPatronBlockConditionsResponse.class, asyncResultHandler);
+  }
+
+  @Override
+  public void postPatronBlockConditions(String lang, PatronBlockCondition entity,
+    Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler,
+    Context vertxContext) {
+
+    PgUtil.post(PATRON_BLOCKS_CONDITIONS, entity, okapiHeaders, vertxContext,
+      PostPatronBlockConditionsResponse.class, asyncResultHandler);
+  }
+
+  @Override
+  public void putPatronBlockConditionsByPatronBlockConditionId(
+    String patronBlockConditionId, String lang, PatronBlockCondition entity,
+    Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler,
+    Context vertxContext) {
+
+    PgUtil.put(PATRON_BLOCKS_CONDITIONS, entity, patronBlockConditionId, okapiHeaders,
+      vertxContext, PutPatronBlockConditionsByPatronBlockConditionIdResponse.class, asyncResultHandler);
+  }
+
+  @Override
+  public void getPatronBlockConditionsByPatronBlockConditionId(
+    String patronBlockConditionId, String lang, Map<String, String> okapiHeaders,
+    Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+
+    PgUtil.getById(PATRON_BLOCKS_CONDITIONS, PatronBlockCondition.class, patronBlockConditionId,
+      okapiHeaders, vertxContext, GetPatronBlockConditionsByPatronBlockConditionIdResponse.class,
+      asyncResultHandler);
+  }
+
+  @Override
+  public void deletePatronBlockConditionsByPatronBlockConditionId(
+    String patronBlockConditionId, String lang, Map<String,
+    String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler,
+    Context vertxContext) {
+
+    PgUtil.deleteById(PATRON_BLOCKS_CONDITIONS, patronBlockConditionId, okapiHeaders,
+      vertxContext, DeletePatronBlockConditionsByPatronBlockConditionIdResponse.class,
+      asyncResultHandler);
+  }
+}
