b2b561354083927e116fd00229d4b9ea06d4e6d7
Pavlo Smahin <Pavlo_Smahin@epam.com>
2020-06-17 10:41:26 +0300

    MODCFIELDS-46 Provide statistics of options usage
    + update RMB to v30.1.0

--- src/main/java/org/folio/service/impl/RecordRepository.java
+++ src/main/java/org/folio/service/impl/RecordRepository.java
@@ -6,12 +6,15 @@ import io.vertx.core.Future;
 
 import org.folio.model.RecordUpdate;
 import org.folio.rest.jaxrs.model.CustomField;
+import org.folio.rest.jaxrs.model.CustomFieldOptionStatistic;
 import org.folio.rest.jaxrs.model.CustomFieldStatistic;
 import org.folio.rest.jaxrs.model.User;
 
 public interface RecordRepository {
 
   Future<CustomFieldStatistic> retrieveStatisticForField(CustomField field, String tenantId);
+
+  Future<CustomFieldOptionStatistic> retrieveStatisticForFieldOption(CustomField field, String optId, String tenantId);
 
   Future<List<User>> findUsersByField(CustomField field, String tenantId);
 
