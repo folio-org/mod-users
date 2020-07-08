75220a7e7cfa841d94500c27024070b72feff2a9
Pavlo Smahin <Pavlo_Smahin@epam.com>
2020-07-08 14:47:07 +0300

    MODUSERS-158 Improve SQL-statements performance

--- src/main/resources/templates/db_scripts/create_departments_triggers.sql
+++ src/main/resources/templates/db_scripts/create_departments_triggers.sql
@@ -1,11 +1,11 @@
-CREATE OR REPLACE FUNCTION process_department_assign() RETURNS TRIGGER
+CREATE OR REPLACE FUNCTION ${myuniversity}_${mymodule}.process_department_assign() RETURNS TRIGGER
 AS $process_department_assign$
   DECLARE
   	dep_id text;
   BEGIN
     IF (TG_OP = 'INSERT' OR TG_OP = 'UPDATE') THEN
       FOR dep_id IN (SELECT jsonb_array_elements_text(NEW.jsonb->'departments')) LOOP
-        IF ((SELECT COUNT(*) FROM departments WHERE id::text = dep_id) != 1) THEN
+        IF ((SELECT COUNT(*) FROM ${myuniversity}_${mymodule}.departments WHERE id::text = dep_id) != 1) THEN
           RAISE foreign_key_violation USING DETAIL = format('Key (departments)=(%s) is not present in table "departments".', dep_id);
         END IF;
       END LOOP;
@@ -14,18 +14,18 @@ AS $process_department_assign$
   END;
 $process_department_assign$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS process_department_assign_trigger ON users CASCADE;
+DROP TRIGGER IF EXISTS process_department_assign_trigger ON ${myuniversity}_${mymodule}.users CASCADE;
 
 CREATE TRIGGER process_department_assign_trigger
-BEFORE INSERT OR UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE process_department_assign();
+BEFORE INSERT OR UPDATE ON ${myuniversity}_${mymodule}.users FOR EACH ROW EXECUTE PROCEDURE ${myuniversity}_${mymodule}.process_department_assign();
 
-CREATE OR REPLACE FUNCTION process_department_delete() RETURNS TRIGGER
+CREATE OR REPLACE FUNCTION ${myuniversity}_${mymodule}.process_department_delete() RETURNS TRIGGER
 AS $process_department_delete$
   DECLARE
     fk_counter integer := 0;
   BEGIN
     IF (TG_OP = 'DELETE') THEN
-      SELECT COUNT(*) INTO fk_counter FROM users WHERE OLD.jsonb->>'id' = ANY (SELECT jsonb_array_elements_text(jsonb->'departments'));
+      SELECT COUNT(*) INTO fk_counter FROM ${myuniversity}_${mymodule}.users WHERE id = (SELECT id FROM ${myuniversity}_${mymodule}.users WHERE OLD.id::text = ANY (SELECT jsonb_array_elements_text(jsonb->'departments')) LIMIT 1);
         IF (fk_counter > 0) THEN
           RAISE foreign_key_violation USING DETAIL = format('Key (id)=(%s) is still referenced from table "users".', OLD.jsonb->>'id');
         END IF;
@@ -34,7 +34,7 @@ AS $process_department_delete$
   END;
 $process_department_delete$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS process_department_delete_trigger ON departments CASCADE;
+DROP TRIGGER IF EXISTS process_department_delete_trigger ON ${myuniversity}_${mymodule}.departments CASCADE;
 
 CREATE TRIGGER process_department_delete_trigger
-BEFORE DELETE ON departments FOR EACH ROW EXECUTE PROCEDURE process_department_delete();
+BEFORE DELETE ON ${myuniversity}_${mymodule}.departments FOR EACH ROW EXECUTE PROCEDURE ${myuniversity}_${mymodule}.process_department_delete();
--- src/main/resources/templates/db_scripts/create_departments_view.sql
+++ src/main/resources/templates/db_scripts/create_departments_view.sql
@@ -1,5 +1,7 @@
-CREATE OR REPLACE VIEW departments_view AS
-  SELECT d.id AS id, jsonb_set(d.jsonb, '{usageNumber}', to_jsonb(COUNT(u.jsonb->>'departments'))) AS jsonb
-  FROM departments d
-  LEFT JOIN users u ON d.jsonb->>'id' = ANY (SELECT jsonb_array_elements_text(u.jsonb->'departments'))
+CREATE INDEX IF NOT EXISTS users_departments_idx_gin ON ${myuniversity}_${mymodule}.users USING GIN ((jsonb -> 'departments'));
+
+CREATE OR REPLACE VIEW ${myuniversity}_${mymodule}.departments_view AS
+  SELECT d.id AS id, jsonb_set(d.jsonb, '{usageNumber}', to_jsonb(COUNT(u))) AS jsonb
+  FROM ${myuniversity}_${mymodule}.departments d
+  LEFT JOIN ${myuniversity}_${mymodule}.users u ON u.jsonb -> 'departments' ? d.id::text
   GROUP BY d.id;
