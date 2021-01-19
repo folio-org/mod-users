ec538179d961365cf212fa7ac4a827ac5fe2bf58
Adam Dickmeiss <adam@indexdata.dk>
2021-01-19 22:12:13 +0100

    Wait properly for async tenant init to complete
    We need to POST, GET (with wait) and DELETE to do it in full.

---
+++ src/test/java/org/folio/rest/utils/TenantInit.java
@@ -0,0 +1,49 @@
+package org.folio.rest.utils;
+
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
+import org.folio.rest.client.TenantClient;
+import org.folio.rest.jaxrs.model.TenantAttributes;
+
+public class TenantInit {
+
+  private TenantInit() {}
+
+  public static Future<Void> init(TenantClient tenantClient, TenantAttributes ta) {
+    Promise<Void> promise = Promise.promise();
+    try {
+      tenantClient.postTenant(ta, res1 -> {
+        if (res1.failed()) {
+          promise.fail(res1.cause());
+          return;
+        }
+        if (res1.result().statusCode() == 204) {
+          promise.complete();
+          return;
+        }
+        JsonObject jsonObject = res1.result().bodyAsJsonObject();
+        tenantClient.getTenantByOperationId(jsonObject.getString("id"), 50000, res2 -> {
+          if (res2.failed()) {
+            promise.fail(res2.cause());
+            return;
+          }
+          if (res2.result().statusCode() != 200) {
+            promise.fail("tenant GET returned " + res2.result().statusCode());
+            return;
+          }
+          tenantClient.deleteTenantByOperationId(jsonObject.getString("id"), res3 -> {
+            if (res3.result().statusCode() != 204) {
+              promise.fail("tenant GET returned " + res3.result().statusCode());
+              return;
+            }
+            promise.complete();
+          });
+        });
+      });
+    } catch (Exception e) {
+      promise.fail(e);
+    }
+    return promise.future();
+  }
+}
