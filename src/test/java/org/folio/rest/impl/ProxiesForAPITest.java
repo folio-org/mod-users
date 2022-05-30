830500782546c7b3198717f8fd7cb20aa1822bf7
Marc Johnson <marc@k-int.co.uk>
2022-05-30 18:07:17 +0100

    Replace argument captor with dynamic mock MODUSERS-309

--- src/test/java/org/folio/rest/impl/ProxiesForAPITest.java
+++ src/test/java/org/folio/rest/impl/ProxiesForAPITest.java
@@ -5,15 +5,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyBoolean;
 import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 import org.folio.rest.jaxrs.model.ProxiesFor;
 import org.folio.rest.persist.Criteria.Criterion;
 import org.folio.rest.persist.PostgresClient;
 import org.folio.rest.persist.interfaces.Results;
+import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
@@ -22,12 +24,23 @@ import io.vertx.core.Handler;
 class ProxiesForAPITest {
   @Test
   void userAndProxyUserComboExistsCanHandlePostgresClientFailure() {
-    PostgresClient postgresClient = mock(PostgresClient.class);
-    Future<Boolean> future = new ProxiesForAPI()
-        .userAndProxyUserComboExists("someUserId", "someProxyUserId", postgresClient);
-    ArgumentCaptor<Handler<AsyncResult<Results<ProxiesFor>>>> handlerCaptor = ArgumentCaptor.forClass(Handler.class);
-    verify(postgresClient).get(anyString(), any(), any(Criterion.class), anyBoolean(), handlerCaptor.capture());
-    handlerCaptor.getValue().handle(Future.failedFuture(new RuntimeException("my exception")));
+    var postgresClient = mock(PostgresClient.class);
+
+    doAnswer((Answer<Void>) ProxiesForAPITest::provideFailedFutureToHandler)
+    .when(postgresClient).get(anyString(), any(), any(Criterion.class), anyBoolean(), any());
+
+    var future = new ProxiesForAPI()
+      .userAndProxyUserComboExists("someUserId", "someProxyUserId", postgresClient);
+
     assertThat(future.cause().getMessage(), is("my exception"));
+  }
+
+  @Nullable
+  private static Void provideFailedFutureToHandler(InvocationOnMock invocationOnMock) {
+    final Handler<AsyncResult<Results<ProxiesFor>>> handler = invocationOnMock.getArgument(4);
+
+    handler.handle(Future.failedFuture(new RuntimeException("my exception")));
+
+    return null;
   }
 }
