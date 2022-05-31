dd5e26df7cc04938cec53f1e740a427d0fd711ae
Marc Johnson <marc@k-int.co.uk>
2022-05-31 09:37:01 +0100

    Use client for creating a new custom field MODUSERS-309

--- src/test/java/org/folio/support/http/CustomFieldsClient.java
+++ src/test/java/org/folio/support/http/CustomFieldsClient.java
@@ -1,7 +1,12 @@
 package org.folio.support.http;
 
+import static io.restassured.http.ContentType.JSON;
+import static java.net.HttpURLConnection.HTTP_CREATED;
+
 import org.folio.support.CustomField;
 import org.folio.support.CustomFields;
+import org.folio.support.User;
+import org.folio.test.util.TokenTestUtil;
 
 public class CustomFieldsClient {
   private final RestAssuredCollectionApiClient<CustomField, CustomFields> client;
@@ -11,8 +16,30 @@ public class CustomFieldsClient {
       defaultHeaders, CustomField.class, CustomFields.class);
   }
 
+  /**
+   * Creates a custom field
+   *
+   * @param customField the custom field to create
+   * @param createdByUser the user needed to create it, as custom fields specifically
+   *                      requires that a user exist and be referenced when creating
+   *                      a custom field this way
+   * @return the created custom field
+   */
+  public CustomField createCustomField(CustomField customField, User createdByUser) {
+    return client.initialSpecification()
+      .header("X-Okapi-Token", TokenTestUtil.generateToken(
+      createdByUser.getUsername(), createdByUser.getId()))
+      .header("X-Okapi-User-Id", createdByUser.getId())
+      .contentType(JSON)
+      .when()
+      .body(customField)
+      .post()
+      .then()
+      .statusCode(HTTP_CREATED)
+      .extract().as(CustomField.class);
+  }
+
   public CustomFields getCustomFields(String cqlQuery) {
     return client.getRecords(cqlQuery);
   }
-
 }
