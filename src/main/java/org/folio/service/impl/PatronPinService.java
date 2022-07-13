287163d9aaac8f7406214e7f09ca3f37bf09979d
Marc Johnson <marc@k-int.co.uk>
2022-07-13 11:01:31 +0100

    Extract method for hashing the pin as a password MODUSERS-319

--- src/main/java/org/folio/service/impl/PatronPinService.java
+++ src/main/java/org/folio/service/impl/PatronPinService.java
@@ -1,8 +1,11 @@
 package org.folio.service.impl;
 
+import static javax.xml.bind.DatatypeConverter.printHexBinary;
+
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
+import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 
@@ -13,20 +16,28 @@ public class PatronPinService {
   private static final Logger logger = LogManager.getLogger(PatronPinService.class);
   public String derivePin(String pin, String userId) {
     try {
-      SecretKeyFactory pbkdf2KeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512") ;
-      PBEKeySpec keySpec = new PBEKeySpec(pin.toCharArray(), // Input character array of password
-                                          userId.getBytes(), // We should add tenant is here also?
-                                          150000, // Iteration count (c)
-                                          64) ; // 256 bits output hashed password
+      // Should the salt also include the tenant?
+      SecretKey derivedPin = hashPassword(pin, userId);
 
-      // Computes hashed password using PBKDF2HMACSHA512 algorithm and provided PBE specs.
-      byte[] pbkdfHashedArray = pbkdf2KeyFactory.generateSecret(keySpec).getEncoded();
-
-      return javax.xml.bind.DatatypeConverter.printHexBinary(pbkdfHashedArray);
+      return printHexBinary(derivedPin.getEncoded());
     }
     catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
       logger.error("Unable to encode pin", e);
       return null;
     }
+  }
+
+  private SecretKey hashPassword(String password, String salt)
+    throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+    final var ITERATION_COUNT = 150000;
+    final var KEY_LENGTH = 64; // 256 bits
+
+    final var keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+
+    final var keySpec = new PBEKeySpec(password.toCharArray(),
+      salt.getBytes(), ITERATION_COUNT, KEY_LENGTH);
+
+    return keyFactory.generateSecret(keySpec);
   }
 }
