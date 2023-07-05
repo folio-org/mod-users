2d004ee129015ec5525ec2fa31f0996d7e6dd76b
Dzmitry_Butramyou <dzmitry_butramyou@epam.com>
2023-07-05 15:13:53 +0400

    [MODUSERS-360] - Code smells refactoring

--- src/main/java/org/folio/rest/impl/UsersAPI.java
+++ src/main/java/org/folio/rest/impl/UsersAPI.java
@@ -15,7 +15,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
