7bafbb9f9aabe70e4523323559c3539fcb484828
Dzmitry_Butramyou <dzmitry_butramyou@epam.com>
2023-09-06 00:13:44 +0400

    [MODUSERS-384] - Added consortiumId field

--- src/main/resources/templates/db_scripts/update_user_tenants_table.sql
+++ src/main/resources/templates/db_scripts/update_user_tenants_table.sql
@@ -4,7 +4,8 @@ ALTER TABLE user_tenant
   ADD COLUMN IF NOT EXISTS mobile_phone_number text,
   ADD COLUMN IF NOT EXISTS phone_number text,
   ADD COLUMN IF NOT EXISTS barcode text,
-  ADD COLUMN IF NOT EXISTS external_system_id text;
+  ADD COLUMN IF NOT EXISTS external_system_id text,
+  ADD COLUMN IF NOT EXISTS consortium_id text;
 
 DROP INDEX IF EXISTS ${myuniversity}_${mymodule}.username_idx;
 CREATE INDEX IF NOT EXISTS username_idx ON user_tenant USING BTREE (LOWER(f_unaccent(username)));
