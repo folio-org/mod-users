a0fef387704996081b1632010be40b9fa20d5e95
Dzmitry_Butramyou <dzmitry_butramyou@epam.com>
2023-09-20 11:28:15 +0400

    [MODCON-92] - refactoring

--- src/main/resources/templates/db_scripts/update_user_type_in_users_table.sql
+++ src/main/resources/templates/db_scripts/update_user_type_in_users_table.sql
@@ -1,4 +1,4 @@
 UPDATE users
 SET jsonb = jsonb_set(jsonb, '{type}', '"system"')
-WHERE jsonb->'personal'->>'lastName' ILIKE 'System%'
+WHERE jsonb->>'username' IN ('mod-search', 'pub-sub', 'mod-innreach', 'data-export-system-user', 'system-user')
 AND NOT jsonb ? 'type';
