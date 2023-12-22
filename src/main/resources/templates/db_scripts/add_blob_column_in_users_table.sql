549b35f842f559e29e283614dc2a41ce330864fe
Adesh_Singh <Adesh_Singh@epam.com>
2023-12-22 13:44:15 +0530

    [MODUSERS-403]-Added table for profile pictures feature

--- src/main/resources/templates/db_scripts/add_blob_column_in_users_table.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE users
-  ADD COLUMN IF NOT EXISTS profile_picture_blob BYTEA;
---
+++ src/main/resources/templates/db_scripts/create_blob_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS profile_picture (
+    profile_picture_blob BYTEA,
+    picture_link TEXT
+);
