e418bcc3fbf791a5faae612e804d95afefa220a9
Adesh_Singh <Adesh_Singh@epam.com>
2024-05-15 15:26:44 +0530

    TEST

--- src/main/java/org/folio/rest/impl/TenantRefAPI.java
+++ src/main/java/org/folio/rest/impl/TenantRefAPI.java
@@ -33,6 +33,11 @@ public class TenantRefAPI extends TenantAPI {
           if (isNew(attributes, "17.3.0")) {
             tl.withIdContent().add("groups-17.3.0", "groups");
           }
+          if (isNew(attributes, "19.3.0")) {
+            System.out.println("I am exe");
+            tl.withIdContent().add("groups-19.3.2", "groups");
+            System.out.println("exec success");
+          }
 
           tl.withKey("loadSample").withLead("sample-data");
           if (isNew(attributes, "15.4.0")) {
