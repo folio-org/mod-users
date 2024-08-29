4b037b93d72a233a75002a386458aea20e30e0a6
MagzhanArtykov <magzhan_artykov@epam.com>
2024-08-29 18:29:51 +0500

    MODUSERS-451 Consume and handle patron update domain events

--- src/main/java/org/folio/verticle/consumers/UserUpdateEventsHandler.java
+++ src/main/java/org/folio/verticle/consumers/UserUpdateEventsHandler.java
@@ -4,10 +4,9 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.folio.event.service.UserTenantService;
 import org.folio.kafka.AsyncRecordHandler;
 import org.folio.kafka.KafkaHeaderUtils;
-import org.folio.rest.jaxrs.model.UserTenant;
+import org.folio.rest.jaxrs.model.UserEvent;
 import org.folio.rest.utils.OkapiConnectionParams;
 
 import io.vertx.core.Future;
@@ -21,11 +20,9 @@ public class UserUpdateEventsHandler implements AsyncRecordHandler<String, Strin
   private static final Logger logger = LogManager.getLogger(UserUpdateEventsHandler.class);
 
   private final Vertx vertx;
-  private final UserTenantService userTenantService;
 
   public UserUpdateEventsHandler(Vertx vertx) {
     this.vertx = vertx;
-    this.userTenantService = new UserTenantService();
   }
 
   @Override
@@ -33,13 +30,8 @@ public class UserUpdateEventsHandler implements AsyncRecordHandler<String, Strin
     logger.info("Handling user update event: {}", record.value());
     List<KafkaHeader> kafkaHeaders = record.headers();
     OkapiConnectionParams okapiConnectionParams = new OkapiConnectionParams(KafkaHeaderUtils.kafkaHeadersToMap(kafkaHeaders), vertx);
-    UserTenant event = new JsonObject(record.value()).mapTo(UserTenant.class);
+    UserEvent event = new JsonObject(record.value()).mapTo(UserEvent.class);
     logger.info("Trying to handle user updates event: {} in tenant: {}", record.value(), okapiConnectionParams.getTenantId());
-    return userTenantService.updateUserTenant(event, okapiConnectionParams.getTenantId(), okapiConnectionParams.getVertx())
-      .map(event.getId())
-      .onSuccess(x -> logger.info("User primary affiliation with event id: {} has been updated for user id: {} with tenant id: {}",
-        event.getId(), event.getUserId(), event.getTenantId()))
-      .onFailure(e -> logger.error("Failed to update user primary affiliation with event id: {} for user id: {} with tenant id: {}",
-        event.getId(), event.getUserId(), event.getTenantId(), e));
+    return Future.succeededFuture(event.getId());
   }
 }
