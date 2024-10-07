61384aee49a5e0abc6e803d884c5027f76ffd18e
Vignesh-Kalyanasundaram <vignesh_kalyansundaram@epam.com>
2024-10-07 17:43:05 +0530

    MODUSERS-476 Creating Merge API

--- src/main/java/org/folio/rest/impl/StagingUsersAPI.java
+++ src/main/java/org/folio/rest/impl/StagingUsersAPI.java
@@ -2,11 +2,15 @@ package org.folio.rest.impl;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import lombok.extern.log4j.Log4j2;
 import org.folio.rest.jaxrs.model.StagingUser;
 import org.folio.rest.jaxrs.model.StagingUserdataCollection;
 import org.folio.rest.jaxrs.model.StagingUsersGetOrder;
 import org.folio.rest.jaxrs.resource.StagingUsers;
+import org.folio.rest.persist.Criteria.Criteria;
+import org.folio.rest.persist.Criteria.Criterion;
 import org.folio.rest.persist.PgUtil;
 
 import javax.ws.rs.Path;
@@ -14,6 +18,7 @@ import javax.ws.rs.core.Response;
 import java.util.Map;
 
 @Path("staging-users")
+@Log4j2
 public class StagingUsersAPI implements StagingUsers {
 
   public static final String STAGING_USERS_TABLE = "staging_users";
@@ -27,7 +32,24 @@ public class StagingUsersAPI implements StagingUsers {
 
   @Override
   public void postStagingUsers(StagingUser entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    throw new UnsupportedOperationException("postStagingUsers api is not supported currently");
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void putStagingUsersMergeById(String id, String userId, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    var criterion = new Criterion(new Criteria().addField("id").setJSONB(false).setOperation("=").setVal(id));
+    PgUtil.postgresClient(vertxContext, okapiHeaders)
+      .withTrans(conn -> conn.get(STAGING_USERS_TABLE, StagingUser.class, criterion)
+        .compose(stagingUser -> {
+          if (!stagingUser.getResults().isEmpty()) {
+            log.info("Staging User :: {}", stagingUser.getResults().get(0));
+          } else {
+            log.info("Staging User is not found");
+          }
+          return Future.succeededFuture(StagingUsers.PutStagingUsersMergeByIdResponse.respond200())
+            .map(Response.class :: cast);
+          }))
+      .onComplete(asyncResultHandler);
   }
 
 
