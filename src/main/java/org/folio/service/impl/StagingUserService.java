6367d9548cbdbe4bfded0ca45c6572c8dca4d576
Vignesh-Kalyanasundaram <vignesh_kalyansundaram@epam.com>
2024-10-09 23:12:14 +0530

    MODUSERS-476 Creating Merge API

--- src/main/java/org/folio/service/impl/StagingUserService.java
+++ src/main/java/org/folio/service/impl/StagingUserService.java
@@ -7,6 +7,8 @@ import org.apache.logging.log4j.Logger;
 import org.folio.rest.jaxrs.model.Address;
 import org.folio.rest.jaxrs.model.AddressInfo;
 import org.folio.rest.jaxrs.model.AddressType;
+import org.folio.rest.jaxrs.model.ContactInfo;
+import org.folio.rest.jaxrs.model.GeneralInfo;
 import org.folio.rest.jaxrs.model.Personal;
 import org.folio.rest.jaxrs.model.StagingUser;
 import org.folio.rest.jaxrs.model.User;
@@ -15,12 +17,13 @@ import org.folio.rest.persist.Conn;
 import org.folio.rest.persist.Criteria.Criteria;
 import org.folio.rest.persist.Criteria.Criterion;
 import org.folio.rest.persist.PgUtil;
+import org.folio.rest.tools.utils.MetadataUtil;
 import org.folio.service.UsersService;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -28,12 +31,10 @@ import static io.vertx.core.Future.succeededFuture;
 import static org.folio.rest.impl.AddressTypeAPI.ADDRESS_TYPE_TABLE;
 import static org.folio.rest.impl.StagingUsersAPI.STAGING_USERS_TABLE;
 import static org.folio.rest.impl.UserGroupAPI.GROUP_TABLE;
+import static org.folio.support.UsersApiConstants.ID;
 
 public class StagingUserService {
 
-  private final Context vertxContext;
-  private final Map<String, String> okapiHeaders;
-  private static final Logger log = LogManager.getLogger(StagingUserService.class);
   public static final String PATRON_TYPE = "patron";
   public static final String CONTACT_TYPE_EMAIL_ID = "002";
   public static final String USER_NOT_FOUND = "user with id %s not found";
@@ -44,14 +45,20 @@ public class StagingUserService {
   public static final String HOME = "Home";
   public static final String GROUP = "group";
   public static final String REMOTE_NON_CIRCULATING = "Remote Non-circulating";
+  private static final Logger log = LogManager.getLogger(StagingUserService.class);
+  private final Context vertxContext;
+  private final Map<String, String> okapiHeaders;
+
   public StagingUserService(Context vertxContext, Map<String, String> okapiHeaders) {
     this.vertxContext = vertxContext;
     this.okapiHeaders = okapiHeaders;
   }
 
   public Future<User> mergeStagingUserWithUser(String stagingUserId, String userId) {
+    log.debug("mergeStagingUserWithUser:: Merging staging user Details with id {} to user with id {}",
+      stagingUserId, userId);
     return PgUtil.postgresClient(vertxContext, okapiHeaders)
-      .withTrans(conn -> Future.all(fetchHomeAddressType(), fetchRemotePatronGroupOnlyIfUserIdPresent(userId))
+      .withTrans(conn -> Future.all(fetchHomeAddressType(conn), fetchRemotePatronGroupOnlyIfUserIdPresent(conn, userId))
         .compose(compositeFuture -> {
           AddressType homeAddress = compositeFuture.resultAt(0);
           String homeAddressId = homeAddress.getId();
@@ -59,53 +66,72 @@ public class StagingUserService {
           Usergroup remotePatronGroup = compositeFuture.resultAt(1);
           String patronGroupId = remotePatronGroup != null ? remotePatronGroup.getId() : null;
 
-          // Fetch the staging user and then process them
-          return fetchStagingUserById(stagingUserId)
+          return fetchStagingUserById(conn, stagingUserId)
             .compose(stagingUser -> handleStagingUser(stagingUser, userId, conn, homeAddressId, patronGroupId));
-        }));
+        }))
+      .onFailure(t -> log.error("mergeStagingUserWithUser:: Merge failed for stagingUserId {}, userId {}: {}",
+        stagingUserId, userId, t.getMessage()));
   }
 
-  private Future<AddressType> fetchHomeAddressType() {
-    return fetchEntityByFieldNameAndValue(ADDRESS_TYPE, HOME, AddressType.class, ADDRESS_TYPE_TABLE,
+  private Future<AddressType> fetchHomeAddressType(Conn conn) {
+    log.debug("fetchHomeAddressType:: fetching address of type home");
+    return fetchEntityByFieldNameAndValue(conn, ADDRESS_TYPE, HOME, AddressType.class, ADDRESS_TYPE_TABLE,
       HOME_ADDRESS_TYPE_NOT_FOUND);
   }
 
-  private Future<Usergroup> fetchRemotePatronGroupOnlyIfUserIdPresent(String userId) {
-    return userId == null
-      ? fetchEntityByFieldNameAndValue(GROUP, REMOTE_NON_CIRCULATING,
+  private Future<Usergroup> fetchRemotePatronGroupOnlyIfUserIdPresent(Conn conn, String userId) {
+    log.debug("fetchRemotePatronGroupOnlyIfUserIdPresent:: fetching Remote patron group with userId {}", userId);
+    return userId != null
+      ? fetchEntityByFieldNameAndValue(conn, GROUP, REMOTE_NON_CIRCULATING,
       Usergroup.class, GROUP_TABLE, REMOTE_PATRON_GROUP_NOT_FOUND)
       : Future.succeededFuture(null);
   }
-  private Future<StagingUser> fetchStagingUserById(String stagingUserId) {
-    return fetchEntityByFieldNameAndValue("id", stagingUserId, StagingUser.class, STAGING_USERS_TABLE, STAGING_USER_NOT_FOUND);
+
+  private Future<StagingUser> fetchStagingUserById(Conn conn, String stagingUserId) {
+    log.debug("fetchStagingUserById:: fetching staging user with id {}", stagingUserId);
+    return fetchEntityByFieldNameAndValue(conn, ID, stagingUserId, StagingUser.class, STAGING_USERS_TABLE, STAGING_USER_NOT_FOUND);
   }
 
-  private <T> Future<T> fetchEntityByFieldNameAndValue(String fieldName, String fieldValue, Class<T> clazz, String tableName, String errorMessage) {
-    var criterion = new Criterion(new Criteria().addField("'"+fieldName+"'")
+  private <T> Future<T> fetchEntityByFieldNameAndValue(Conn conn, String fieldName, String fieldValue, Class<T> clazz, String tableName, String errorMessage) {
+    log.debug("fetchEntityByFieldNameAndValue:: Fetching entity {} with field {} and value {}",
+      clazz.getSimpleName(), fieldName, fieldValue);
+    var criterion = new Criterion(new Criteria().addField("'" + fieldName + "'")
       .setOperation("=").setVal(fieldValue));
-    return PgUtil.postgresClient(vertxContext, okapiHeaders)
-      .withTrans(conn -> conn.get(tableName, clazz, criterion))
+    return conn.get(tableName, clazz, criterion)
       .compose(results -> {
         if (!results.getResults().isEmpty()) {
           return succeededFuture(results.getResults().get(0));
         } else {
+          log.warn("fetchEntityByFieldNameAndValue:: Entity not found: {} with field {} and value: {}",
+            clazz.getSimpleName(), fieldName, fieldValue);
           return Future.failedFuture(String.format(errorMessage, fieldValue));
         }
       })
-      .onFailure(t -> log.error("fetchEntityByFieldNameAndValue:: Unable to fetch the entity with fieldName {} , " +
-        "field value {} from table {}", fieldName, fieldValue, tableName, t));
+      .onFailure(t -> log.error("fetchEntityByFieldNameAndValue:: Error fetching {} from table {}: {}",
+        clazz.getSimpleName(), tableName, t.getMessage()));
   }
 
-  public Future<User> handleStagingUser(StagingUser stagUser, String userId, Conn conn, String homeAddressTypeId, String patronGroupId) {
+  public Future<User> handleStagingUser(StagingUser stagUser, String userId, Conn conn, String homeAddressTypeId,
+                                        String patronGroupId) {
+    log.debug("handleStagingUser:: handle staging user {} with userId {} , homeAddressId {} , patronGroupId {}",
+      stagUser, userId, homeAddressTypeId, patronGroupId);
     var userService = new UsersService();
     if (userId == null) {
+      log.info("handleStagingUser:: User id is null, hence creating a new user from staging user with id {}",
+        stagUser.getId());
       var newUser = createUserFromStagingUser(stagUser, homeAddressTypeId, patronGroupId);
       return userService.saveAndReturnUser(conn, newUser);
     } else {
+      log.info("handleStagingUser:: Fetching userId {} and updating the user from staging user with id {}",
+        userId, stagUser.getId());
       return userService.getUserById(conn, userId)
         .compose(existingUser -> {
-          var updatedUser = updateUserFromStagingUser(stagUser, existingUser, homeAddressTypeId);
-          return userService.updateUser(conn, updatedUser);
+          if (existingUser != null) {
+            var updatedUser = updateUserFromStagingUser(stagUser, existingUser, homeAddressTypeId);
+            return userService.updateUser(conn, updatedUser);
+          } else {
+            return Future.failedFuture(String.format(USER_NOT_FOUND, userId));
+          }
         });
     }
   }
@@ -125,53 +151,62 @@ public class StagingUserService {
     user.setEnrollmentDate(stagingUser.getMetadata().getUpdatedDate());
     Personal personal = createOrUpdatePersonal(stagingUser, new Personal(), homeAddressTypeId);
     user.setPersonal(personal);
-
+    user.setMetadata(MetadataUtil.createMetadata(okapiHeaders));
     return user;
   }
 
   private User updateUserFromStagingUser(StagingUser stagingUser, User user, String homeAddressTypeId) {
     user.setActive(true);
+    user.setExternalSystemId(stagingUser.getContactInfo() != null ? stagingUser.getContactInfo().getEmail() : null);
     user.setEnrollmentDate(stagingUser.getMetadata().getUpdatedDate());
     Personal personal = createOrUpdatePersonal(stagingUser, user.getPersonal(), homeAddressTypeId);
     user.setPersonal(personal);
     return user;
   }
+
   private Personal createOrUpdatePersonal(StagingUser stagingUser, Personal personal, String homeAddressTypeId) {
     var contactInfo = stagingUser.getContactInfo();
     var generalInfo = stagingUser.getGeneralInfo();
     var addressInfo = stagingUser.getAddressInfo();
 
+    setContactInfo(contactInfo, personal);
+    setGeneralInfo(generalInfo, personal);
+    Address homeAddress = personal.getAddresses() != null
+      ? personal.getAddresses().stream()
+      .filter(address -> homeAddressTypeId.equals(address.getAddressTypeId()))
+      .findFirst()
+      .orElse(new Address())
+      : new Address();
+
+    updateAddress(homeAddress, addressInfo, homeAddressTypeId);
+    if (personal.getAddresses() == null) {
+      personal.setAddresses(new ArrayList<>());
+    }
+    if (!personal.getAddresses().contains(homeAddress)) {
+      personal.getAddresses().add(homeAddress);
+    }
+
+    return personal;
+  }
+
+  private void setContactInfo(ContactInfo contactInfo, Personal personal) {
     if (contactInfo != null) {
       personal.setEmail(contactInfo.getEmail());
       personal.setPhone(contactInfo.getPhone());
       personal.setMobilePhone(contactInfo.getMobilePhone());
       personal.setPreferredContactTypeId(CONTACT_TYPE_EMAIL_ID);
     }
+  }
+
+  private void setGeneralInfo(GeneralInfo generalInfo, Personal personal) {
     if (generalInfo != null) {
       personal.setFirstName(generalInfo.getFirstName());
       personal.setLastName(generalInfo.getLastName());
       personal.setMiddleName(generalInfo.getMiddleName());
       personal.setPreferredFirstName(generalInfo.getPreferredFirstName());
     }
-    if (addressInfo != null) {
-      Address userAddress = personal.getAddresses() != null
-        ? personal.getAddresses()
-        .stream()
-        .filter(address -> homeAddressTypeId.equals(address.getAddressTypeId()))
-        .findFirst()
-        .orElse(new Address())
-        : new Address();
-
-      updateAddress(userAddress, addressInfo, homeAddressTypeId);
-      if (personal.getAddresses() == null) {
-        personal.setAddresses(List.of(userAddress));
-      } else if (!personal.getAddresses().contains(userAddress)) {
-        personal.getAddresses().add(userAddress);
-      }
-    }
-
-    return personal;
   }
+
   private void updateAddress(Address userAddress, AddressInfo addressInfo, String homeAddressTypeId) {
     if (userAddress.getId() == null) {
       userAddress.setId(UUID.randomUUID().toString());
@@ -185,6 +220,4 @@ public class StagingUserService {
     userAddress.setAddressTypeId(homeAddressTypeId);
     userAddress.setPrimaryAddress(true);
   }
-
-
 }
