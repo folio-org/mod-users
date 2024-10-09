25d4d8491a9eea10e0b51f01dee2be8b7031478a
Kapil Verma <kapil_verma1@epam.com>
2024-10-09 17:53:57 +0530

    MODUSERS-475: added upsert operation in StagingUser POST API

--- src/main/java/org/folio/rest/impl/StagingUsersAPI.java
+++ src/main/java/org/folio/rest/impl/StagingUsersAPI.java
@@ -6,34 +6,22 @@ import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.folio.cql2pgjson.CQL2PgJSON;
-import org.folio.cql2pgjson.exception.CQL2PgJSONException;
-import org.folio.rest.jaxrs.model.ProxiesFor;
 import org.folio.rest.jaxrs.model.StagingUser;
 import org.folio.rest.jaxrs.model.StagingUserdataCollection;
 import org.folio.rest.jaxrs.model.StagingUsersGetOrder;
-import org.folio.rest.jaxrs.model.User;
 import org.folio.rest.jaxrs.resource.StagingUsers;
 import org.folio.rest.persist.Criteria.Criteria;
 import org.folio.rest.persist.Criteria.Criterion;
-import org.folio.rest.persist.Criteria.Limit;
-import org.folio.rest.persist.Criteria.Offset;
 import org.folio.rest.persist.PgUtil;
 import org.folio.rest.persist.PostgresClient;
-import org.folio.rest.persist.cql.CQLWrapper;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import static io.vertx.core.Future.failedFuture;
 import static io.vertx.core.Future.succeededFuture;
-import static java.util.Collections.emptyList;
-import static org.folio.support.UsersApiConstants.TABLE_NAME_USERS;
-import static org.folio.support.UsersApiConstants.VIEW_NAME_USER_GROUPS_JOIN;
 
 @Path("staging-users")
 public class StagingUsersAPI implements StagingUsers {
@@ -51,9 +39,8 @@ public class StagingUsersAPI implements StagingUsers {
   public void postStagingUsers(StagingUser entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     PostgresClient postgresClient = PgUtil.postgresClient(vertxContext, okapiHeaders);
     final Criterion criterion = new Criterion(
-      new Criteria().addField("contactInfo.email")
+      new Criteria().addField("'contactInfo'->>'email'")
         .setOperation("=").setVal(entity.getContactInfo().getEmail()).setJSONB(true));
-
     postgresClient.withTrans(conn -> {
       return conn.get(STAGING_USERS_TABLE, StagingUser.class, criterion, true)
         .compose(stagingUserResults -> {
