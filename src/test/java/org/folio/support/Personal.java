368e8bd2b7e1740723b7a91354c59b973ed95254
Kapil Verma <85541312+kapil-epam@users.noreply.github.com>
2025-01-15 15:15:47 +0530

    MODUSERS-454: Extended User Record Schema to add Pronouns field (#398)
    * MODUSERS-454: Extended User Record Schema to add Pronouns field
    
    * MODUSERS-454: upgraded dependency interface minor version 16.3 to 16.4 for users api

--- src/test/java/org/folio/support/Personal.java
+++ src/test/java/org/folio/support/Personal.java
@@ -13,6 +13,7 @@ import lombok.extern.jackson.Jacksonized;
 @Jacksonized
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Personal {
+  String pronouns;
   String lastName;
   String firstName;
   String middleName;
