5f9eafdcd252b5c9fcbcd468b37524d39028dc46
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2026-01-07 18:30:38 +0500

    [MODUSERS-535]. Increase partition size to 5 to match value set in mod-consortia-keycloak

--- src/main/java/org/folio/support/kafka/topic/UsersKafkaTopic.java
+++ src/main/java/org/folio/support/kafka/topic/UsersKafkaTopic.java
@@ -5,9 +5,9 @@ import org.folio.kafka.services.KafkaTopic;
 public enum UsersKafkaTopic implements KafkaTopic {
   USER_GROUP("userGroup", 10),
   USERS("users", 10),
-  USER_CREATED("USER_CREATED", 3),
-  USER_UPDATED("USER_UPDATED", 3),
-  USER_DELETED("USER_DELETED", 3);
+  USER_CREATED("USER_CREATED", 5),
+  USER_UPDATED("USER_UPDATED", 5),
+  USER_DELETED("USER_DELETED", 5);
 
   private final String topic;
   private final int partitions;
