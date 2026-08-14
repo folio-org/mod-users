3e8c2b7ab69587cd8a86e27a9762f895253ab35e
oleksandr_vidinieiev <oleksandr_vidinieiev@epam.com>
2026-08-14 11:59:15 +0200

    Enhance ConfigurationClientImpl with Lombok's @RequiredArgsConstructor

--- src/main/java/org/folio/client/impl/ConfigurationClientImpl.java
+++ src/main/java/org/folio/client/impl/ConfigurationClientImpl.java
@@ -13,7 +13,9 @@ import org.folio.rest.jaxrs.model.ConfigurationEntry;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class ConfigurationClientImpl implements ConfigurationClient {
 
   private static final String CONFIGS_ARRAY_NAME = "configs";
@@ -24,11 +26,6 @@ public class ConfigurationClientImpl implements ConfigurationClient {
 
   private final VertxOkapiHttpClient client;
   private final Map<String, String> headers;
-
-  public ConfigurationClientImpl(VertxOkapiHttpClient client, Map<String, String> headers) {
-    this.client = client;
-    this.headers = headers;
-  }
 
   @Override
   public Future<ConfigurationEntry> getConfiguration(String module, String configName) {
